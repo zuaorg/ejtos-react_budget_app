bb4874d5258d31c0b8a5a71b77c2b892451796a9
zuaorg <zua.org@gmail.com>
2023-05-08 15:40:06 -0400

    budget component changes

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,30 @@
-import React, { useState } from 'react';
-//import { AppContext } from '../context/AppContext';
-const Budget = () => {
-    const { budget } = useState('');
+import React, { useState,useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = (props) => {
+    const { dispatch,totalExpenses } = useContext(AppContext);
+    const [budget,setBudget] = useState("");
+
+    const submitEvent = () => {
+
+        if(budget > 20000) {
+            alert("The value cannot exceed 20000");
+            setBudget("");
+            return;
+        }
+        else{
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: budget,
+            });
+        }
+    }
+
     return (
         <div className='alert alert-secondary'>
             <label for="edtbudget">Budget: £</label>
-            <input id="edtbudget" type="number" step="10" value={budget}
-            onChange></input>
+            <input id="edtbudget" type="number" step="10" value={budget} 
+            onChange = {(event) => setBudget(event.target.value)}
+            onfocusout = {(submitEvent)}></input>
         </div>
     );
 };
